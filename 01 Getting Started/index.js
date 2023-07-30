95cce4652045815a05728ee3cefccd37a170612a
Bernd Barsuhn <bernd.barsuhn@me.com>
2023-07-30 21:58:15 +0200

    Added HTML and CSS chapters

--- 01 Getting Started/index.js
+++ 01 Getting Started/index.js
@@ -2,16 +2,18 @@ console.log('JavaScript läuft');
 
 let content = document.getElementById('content');
 let sample = document.getElementById('sample');
+let x = document.getElementsByClassName('highlighted');
+let y = document.getElementsByTagName('p');
 
 content.textContent = "Dieser Text wurde von einem JavaScript Programm eingefügt.";
 
-for (let i=0; i < 10; ++i)
+let count = prompt('Wie viele kopien des Textes sollen angezeigt werden', 0);
+
+for (let i=0; i < count; ++i)
 {
     let clone = sample.cloneNode(true);
     clone.removeAttribute('id');
-    
     content.appendChild(clone);    
 }
 
-console.log(sample);
-console.log(content);
+console.log(x);
