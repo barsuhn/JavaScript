f518ed13b2abe216ff45f7f7ffe3096572cdebcd
Bernd Barsuhn <bernd.barsuhn@me.com>
2023-12-31 17:09:34 +0100

    Simplified the first Node example.

--- 04-Node/prompt.js
+++ 04-Node/prompt.js
@@ -1,12 +1,3 @@
-import createPrompt from "prompt-sync";
-
-const prompt = createPrompt();
-const print = console.log;
-
-async function main() {
-  let name = prompt("What is your name? ");
-
-  print(`Hello ${name}!`);
-}
-
-await main();
+import createPrompt from "prompt-sync"
+let name = createPrompt()("What is your name? ")
+console.log(`Hello ${name}!`)
